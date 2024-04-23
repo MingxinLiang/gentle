58cedf31654aaf7ce6486153c542bfe54efeb48c
soldate <soldate@live.com>
2024-04-23 03:29:53 +0000

    update:centos

--- ext/install_kaldi.sh
+++ ext/install_kaldi.sh
@@ -7,6 +7,7 @@ make
 ./extras/install_openblas.sh
 cd ../src
 # make clean (sometimes helpful after upgrading upstream?)
-./configure --static --static-math=yes --static-fst=yes --use-cuda=no --openblas-root=../tools/OpenBLAS/install
+#./configure --static --static-math=yes --static-fst=yes --use-cuda=no --openblas-root=../tools/OpenBLAS/install
+./configure --static --static-math=yes --static-fst=yes --use-cuda=no
 make depend
 cd ../../
--- install_deps.sh
+++ install_deps.sh
@@ -6,11 +6,15 @@ echo "Installing dependencies..."
 
 # Install OS-specific dependencies
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-	apt-get update -qq
-	apt-get install -y zlib1g-dev automake autoconf git \
+	#apt-get update -qq
+	#apt-get install -y zlib1g-dev automake autoconf git \
+	#	libtool subversion libatlas3-base python3-pip \
+	#	python3-dev wget unzip python3
+	#apt-get install -y ffmpeg || echo -n  "\n\nYou have to install ffmpeg from a PPA or from https://ffmpeg.org before you can run gentle\n\n"
+	yum install -y zlib zlib-devel automake autoconf git \
 		libtool subversion libatlas3-base python3-pip \
-		python3-dev wget unzip python3
-	apt-get install -y ffmpeg || echo -n  "\n\nYou have to install ffmpeg from a PPA or from https://ffmpeg.org before you can run gentle\n\n"
+		python3-devel wget unzip python3
+	yum install -y ffmpeg || echo -n  "\n\nYou have to install ffmpeg from a PPA or from https://ffmpeg.org before you can run gentle\n\n"
 	python3 setup.py develop
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 	brew install ffmpeg libtool automake autoconf wget python3
